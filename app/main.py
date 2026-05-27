0c2cd722f8a780ba0fc82aac16d740b2160a7a93
Ubuntu <ubuntu@ip-172-31-4-212.ap-south-1.compute.internal>
2026-05-27 09:04:42 +0000

    first commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import redis
+import os
+
+app = FastAPI()
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=6379,
+    decode_responses=True
+)
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI Production App Running"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
