298d6074a022af1c32f167acd295d7c59d2a4c8c
Nikhilsingh9889 <Nikhilsingh2002nik@gmail.com>
2026-05-27 17:33:03 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ redis_client = redis.Redis(
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI Production App Running"}
+    return {"message": "FastAPI Production Mobile App Running"}
 
 @app.get("/health")
 def health():
