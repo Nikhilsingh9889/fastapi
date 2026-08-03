78f2f4e8f826eaac32cfe4d7bd0c025985a77530
Nikhilsingh9889 <Nikhilsingh2002nik@gmail.com>
2026-08-03 18:43:41 +0530

    Update home endpoint message for mobile website

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ redis_client = redis.Redis(
 
 @app.get("/")
 def home():
-    return {"message": "Mobile app is Running"}
+    return {"message": "Mobile Website is Running"}
 
 @app.get("/health")
 def health():
