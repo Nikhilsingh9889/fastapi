1d40ed2c8c1e5746c120472ef2fd1caa848db610
Nikhilsingh9889 <Nikhilsingh2002nik@gmail.com>
2026-08-03 16:32:30 +0530

    Change home message to 'Mobile app is Running'

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ redis_client = redis.Redis(
 
 @app.get("/")
 def home():
-    return {"message": "Mobile web is Running"}
+    return {"message": "Mobile app is Running"}
 
 @app.get("/health")
 def health():
